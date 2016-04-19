4dedd7418ef6a821a8b381699a584bef2d0ee8d6
Willson Smith <me@willsonsmith.com>
2016-04-18 21:44:56 -0400

    some more robust handling of file types

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
 import markdown
 
-
 class Generator:
     def __init__(self, template='', source_dir='source', output_dir='output'):
         current_path = os.path.dirname(os.path.abspath(__file__))
@@ -15,6 +14,7 @@ class Generator:
         self.template = template
         self.source_dir = source_dir
         self.output_dir = output_dir
+        self.extensions = ('*.txt', '*.md', '*.markdown', '*.html')
 
     def _output_file(self, source):
         file_name = os.path.basename(source)
@@ -22,13 +22,25 @@ class Generator:
             os.makedirs(self.output_dir)
         return open(self.output_dir + '/' + file_name + '.html', 'w+')
 
+    def _files_with_extensions(self, extensions):
+        list_of_files = []
+        for extension in extensions:
+            list_of_files.extend(glob.glob(self.source_dir + '/' + extension))
+        return list_of_files
+
+    def _handle_markdown(self, file_name, source):
+        file_extension = os.path.splitext(file_name)[1]
+        if file_extension == '.md':
+            return markdown.markdown(source)
+        return source
+
     def build_template(self):
-        sources = glob.glob(self.source_dir + '/*.md')
-        for source in sources:
+        list_of_files = self._files_with_extensions(self.extensions)
+        for source in list_of_files:
             templ = self.env.get_template(self.template)
             saved_file = self._output_file(source)
             source_content = open(source, 'r').read()
-            source_content = markdown.markdown(source_content)
+            source_content = self._handle_markdown(source, source_content)
             saved_file.write(templ.render(source=source_content))
 
 if __name__ == "__main__":
